4e2c5566a8895105d1bfb2d33143db2f7c532b2d
赵文明 <zwm2007@126.com>
2019-08-26 18:34:40 +0800

    xinzeng21

--- common/readExcel.py
+++ common/readExcel.py
@@ -24,3 +24,30 @@ def col_value(fp,sheet_name,col):
     test_data=xlrd.open_workbook(fp)
     sheet_name=test_data.sheet_by_name(sheet_name)
     return sheet_name.col_values(col-1)
+
+
+def read_all_req(fp,sheet_name):
+    print("fp:",fp)
+    test_data=xlrd.open_workbook(fp)
+    sheet_name=test_data.sheet_by_name(sheet_name)
+    row=sheet_name.nrows
+    print(row)
+    col=sheet_name.ncols
+    print(col)
+
+    param_key =sheet_name.row_values(0)
+    for i in range(len(param_key)):
+        if param_key[i] == 'Response':
+            req_len = i
+            break
+
+    data_list=[]
+    for i in range(1,row):
+        dict={}
+        for j in range(1,col):
+            dict[sheet_name.cell_value(0,j)]=sheet_name.cell_value(i,j)
+        data_list.append(dict)
+    return data_list
+
+if __name__=='__main__':
+    read_all_req()
